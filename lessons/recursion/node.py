346e27b65fb2ec52245f7ca792ac199847841895
Jaitan <jaitan@unc.edu>
2023-11-18 16:49:20 -0500

    10/18

--- lessons/recursion/node.py
+++ lessons/recursion/node.py
@@ -3,6 +3,9 @@
 from __future__ import annotations
 
 
+__author__ = "730675117"
+
+
 class Node:
     """My Node class for linked lists."""
     
@@ -12,7 +15,7 @@ class Node:
     def __init__(self, data: int, next: Node | None):
         """Construct Node."""
         self.data = data
-        self. next = next
+        self.next = next
         
     def __str__(self) -> str:
         """Produce a string visualization of the linked list."""
@@ -22,11 +25,20 @@ class Node:
         else:
             return f"{self.data} -> {self.next}"
         
-    def head(self):
-        return None
+    def head(self) -> int:
+        """Part 0."""
+        return int(self.data)
     
-    def tail(self):
-        return None
+    def tail(self) -> Node | None:
+        """Part 1."""
+        return self.next
     
-    def last(self):
-        return None
+    def last(self) -> int:
+        """Part 2."""
+        if self.next is None: 
+            return self.data
+        if self.next.next is None: 
+            return self.next.data
+        if self.next.next.next is None: 
+            return self.next.next.data
+        return int(0)
